61ad9a1e125d37fe881a13b325dd34571ede0879
Madeleine Filloux <mfilloux@gmail.com>
2018-03-11 19:42:49 -0500

    cleaned up JS and CSS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,54 @@
 /*---------------------------
     GLOBAL VARIABLES
 ---------------------------*/
-let startingHP = [90, 100, 110, 120];
-let startingAttackPower = [4, 6, 8, 10];
-let character0 = new Character("Donald Trump", "US", 90, 4, 20);
-let character1 = new Character("Vladimir Putin", "RU", 100, 6, 25);
-let character2 = new Character("Kim Jong Un", "KP", 110, 8, 30);
-let character3 = new Character("Xi Jinping", "CN", 120, 10, 35);
-const characters = [character0, character1, character2, character3];
+let names = ["Donald Trump", "Vladimir Putin", "Kim Jong Un", "Xi Jinping"];
+let countries = ["US","RU","KP","CN"];
+let startingHP = [120, 180, 100, 150];
+let startingAttackPower = [8, 8, 30, 20];
+let startingCounter = [15, 25, 5, 20];
+let characters = [];
 
 let game = {
     userCharacter: '',
     opponent: '',
-    enemies: [character0, character1, character2, character3],
+    enemies: [],
     startGame: function () {
+        // UPDATE THE DOM WITH CHARACTER INFO
         characters.forEach(function(elem, i) {
-            elem.healthPoints = startingHP[i];
-            elem.attackPower = startingAttackPower[i];
-            elem.baseAttackPower = startingAttackPower[i];
             $(`img:eq(${i})`).attr("src",`assets/images/${elem.name.toLowerCase()}.png`);
             $(`img:eq(${i})`).attr("alt",`cartoon of ${elem.name}`);
             $(`.health:eq(${i})`).text(elem.healthPoints);  
             $(`.figure-caption:eq(${i})`).text(elem.name);
             $(`.flag-icon:eq(${i})`).addClass(`flag-icon-${elem.country.toLowerCase()}`);
+            game.enemies.push(elem);
         });
     },
     pickCharacters: function (charDiv) {
-        // PICK USER CHARACTER
         let clickedChar = characters[charDiv.attr("value")];
+        this.enemies.splice(this.enemies.indexOf(clickedChar), 1);
+        // PICK USER CHARACTER
         if (!this.userCharacter) {
             this.userCharacter = clickedChar;
             charDiv.attr("id", "user-character");
-            this.enemies.splice(this.enemies.indexOf(clickedChar), 1);
             $(".character:not([id])").each(function (i) {
                 $(this).addClass("enemy");
                 $(`#select-opponent .col-7:nth-child(${i + 1})`).append($(this));
             });
-            $("#select-character").addClass("d-none");
-            $("#select-opponent").removeClass("d-none");
+            $("#select-character, #select-opponent").toggleClass("d-none");
             $("#character-container").append(charDiv);
         }
         // PICK OPPONENT
         else if (!this.opponent) {
             this.opponent = clickedChar;
             charDiv.attr("id", "opponent");
-            this.enemies.splice(this.enemies.indexOf(clickedChar), 1);
             $(".enemy:not([id])").each(function (i) {
                 $(`#enemy-container .col-3:nth-child(${i + 1})`).append($(this));
             });
-            $("#opponent-container").append(charDiv);
             $("#selection-container").addClass("d-none");
             $("#play-container").removeClass("d-none");
             $("#attack-outcome").text('');
             $("#attack-outcome").removeClass("bg-secondary bg-success");
+            $("#opponent-container").append(charDiv);
             formatCircleType();
         }
     },
@@ -62,47 +58,53 @@ let game = {
             $("#attack-outcome").text("No enemy here.\nSelect the enemy you will fight next.");
         }
         else {
-            let attackSummary = `You attacked ${this.opponent.name} for ${this.userCharacter.attackPower} damage.\n${this.opponent.name} attacked you back for ${this.opponent.counterPower} damage.`;
             this.opponent.healthPoints -= this.userCharacter.attackPower;
             $(`#opponent .health`).text(this.opponent.healthPoints);
             this.userCharacter.healthPoints -= this.opponent.counterPower;
             $(`#user-character .health`).text(this.userCharacter.healthPoints);
-            this.userCharacter.attackPower += this.userCharacter.baseAttackPower;
             // USER CHARACTER KILLED
             if (!this.userCharacter.checkAlive()) {
                 $("#attack-outcome").removeClass("bg-danger bg-success").addClass("bg-secondary");
                 $("#attack-outcome").text(`You've been defeated by ${this.opponent.name}.\nGame Over!`);
-                $("#btn-attack").addClass("d-none");
-                $("#btn-reset").removeClass("d-none");
+                $("#btn-attack, #btn-reset").toggleClass("d-none");
             }
             // OPPONENT KILLED
             else if (!this.opponent.checkAlive()) {
+                // TODO: CHECK IF GAME OVER
                 $("#attack-outcome").removeClass("bg-danger bg-secondary").addClass("bg-success");
-                $("#attack-outcome").text(`You have defeated ${this.opponent.name}.\nSelect the enemy you will fight next.`);
                 $("#opponent").removeAttr("id").addClass("d-none");
+                if (this.enemies.length > 0) {
+                    $("#attack-outcome").text(`You have defeated ${this.opponent.name}.\nSelect the enemy you will fight next.`);
+                }
+                else {
+                    $("#attack-outcome").text('You are the victorious and supreme leader of the world.\nGame Over.');
+                    $("#btn-attack, #btn-reset").toggleClass("d-none");
+                }
                 this.opponent = '';
             }
+            // ATTACK DIDN'T KILL ANYONE
             else {
                 $("#attack-outcome").removeClass("bg-success bg-secondary").addClass("bg-danger");
-                $("#attack-outcome").text(attackSummary);
+                $("#attack-outcome").text(`You attacked ${this.opponent.name} for ${this.userCharacter.attackPower} damage.\n${this.opponent.name} attacked you back for ${this.opponent.counterPower} damage.`);
             }
+            this.userCharacter.attackPower += this.userCharacter.baseAttackPower;
         }
     },
     resetGame: function () {
+        characters.forEach(function(elem, i) {
+            elem.healthPoints = startingHP[i];
+            elem.attackPower = startingAttackPower[i];
+            elem.baseAttackPower = startingAttackPower[i];
+        });
         this.userCharacter = '';
         this.opponent = '';
-        this.enemies = [character0, character1, character2, character3];
+        this.enemies = [];
         $("#attack-outcome").text('');
         $(".character").removeAttr("id").removeClass("enemy d-none");
         for (let i = 0; i < characters.length; i++) {
             $(`#select-character .col-7:eq(${i})`).append($(`.character[value=${i}]`));
         }
-        $("#btn-reset").addClass("d-none");
-        $("#btn-attack").removeClass("d-none");
-        $("#select-opponent").addClass("d-none");
-        $("#play-container").addClass("d-none");
-        $("#selection-container").removeClass("d-none");
-        $("#select-character").removeClass("d-none");
+        $("#btn-reset, #btn-attack, #select-opponent, #play-container, #selection-container, #select-character").toggleClass("d-none");
         $("#attack-outcome").removeClass("bg-secondary bg-success");
         this.startGame();
         formatCircleType();
@@ -163,6 +165,10 @@ $("#btn-reset").on("click", function () {
 })
 
 $( document ).ready(function() {
+    // INITIALIZE CHARACTERS
+    names.forEach(function(elem, i) {
+        characters.push(new Character(names[i], countries[i], startingHP[i], startingAttackPower[i], startingCounter[i]));
+    });
     game.startGame();
     formatCircleType();
 });
